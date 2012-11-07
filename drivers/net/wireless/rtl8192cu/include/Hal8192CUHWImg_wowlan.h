8c96cf847ec828b8c3cfb5f0c8dcb310807b238b
Emilio López <turl@linux-sunxi.org>
2012-11-07 15:36:29 +0000

    net: rtl8192cu: update
    This one is irrelevant, we can get the latest release from
    Realtek's site.

--- drivers/net/wireless/rtl8192cu/include/Hal8192CUHWImg_wowlan.h
+++ drivers/net/wireless/rtl8192cu/include/Hal8192CUHWImg_wowlan.h
@@ -1,7 +1,7 @@
 /******************************************************************************
  *
  * Copyright(c) 2007 - 2011 Realtek Corporation. All rights reserved.
- *                                        
+ *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of version 2 of the GNU General Public License as
  * published by the Free Software Foundation.
@@ -17,28 +17,18 @@
  *
  *
  ******************************************************************************/
-#ifndef	__MLME_OSDEP_H_
-#define __MLME_OSDEP_H_
+#ifndef __INC_HAL8192CU_FW_IMG_WOWLAN_H
+#define __INC_HAL8192CU_FW_IMG_WOWLAN_H
 
-#include <drv_conf.h>
-#include <osdep_service.h>
-#include <drv_types.h>
+/*Created on  2011/11/ 8, 14:15*/
 
-#if defined(PLATFORM_WINDOWS) || defined(PLATFORM_MPIXEL)
-extern int time_after(u32 now, u32 old);
-#endif
 
-extern void rtw_init_mlme_timer(_adapter *padapter);
-extern void rtw_os_indicate_disconnect( _adapter *adapter );
-extern void rtw_os_indicate_connect( _adapter *adapter );
-void rtw_os_indicate_scan_done( _adapter *padapter, bool aborted);
-extern void rtw_report_sec_ie(_adapter *adapter,u8 authmode,u8 *sec_ie);
+#define TSMCWWImgArrayLength 15044
+extern u8 Rtl8192CUFwTSMCWWImgArray[TSMCWWImgArrayLength];
+#define UMCACutWWImgArrayLength 15044
+extern u8 Rtl8192CUFwUMCACutWWImgArray[UMCACutWWImgArrayLength];
+#define UMCBCutWWImgArrayLength 15024
+extern u8 Rtl8192CUFwUMCBCutWWImgArray[UMCBCutWWImgArrayLength];
 
-#ifdef CONFIG_AP_MODE
-void rtw_indicate_sta_assoc_event(_adapter *padapter, struct sta_info *psta);
-void rtw_indicate_sta_disassoc_event(_adapter *padapter, struct sta_info *psta);
-#endif
-void rtw_reset_securitypriv( _adapter *adapter );
-
-#endif	//_MLME_OSDEP_H_
+#endif //__INC_HAL8192CU_FW_IMG_WOWLAN_H
 
